6185105d6b6ecac5f0caf8b54ca50f6e14e51b6f
Bruno Pereira <brunno.pereira7@gmail.com>
2021-12-20 11:05:18 -0400

    Create

--- pdf-merger.py
+++ pdf-merger.py
@@ -1,14 +1,7 @@
 import PyPDF2 
- 
-pdf1File = open('_files/pdf_1.pdf', 'rb')
-pdf2File = open('_files/pdf_2.pdf', 'rb')
-pdf3File = open('_files/pdf_3.pdf', 'rb')
 
 filesName = ['_files/pdf_1.pdf', '_files/pdf_2.pdf', '_files/pdf_3.pdf']
 
-pdf2Reader = PyPDF2.PdfFileReader(pdf2File)
-pdf3Reader = PyPDF2.PdfFileReader(pdf3File)
- 
 pdfWriter = PyPDF2.PdfFileWriter()
 for pdfFile in filesName:
   pdfReader = PyPDF2.PdfFileReader(pdfFile)
@@ -17,7 +10,7 @@ for pdfFile in filesName:
     pdfWriter.addPage(pageObj)
 
  
-pdfOutputFile = open('MergedFiles.pdf', 'wb')
+pdfOutputFile = open('_files/MergedFiles.pdf', 'wb')
 pdfWriter.write(pdfOutputFile)
  
 pdfOutputFile.close()
